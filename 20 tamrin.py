f0bd5f4652b741f452816a8d6ba0c5de0e5def5f
A-Mohseni <mohsnyali724@gmail.com>
2025-07-14 15:41:29 +0330

    add lesson 20

---
+++ 20 tamrin.py
@@ -0,0 +1,35 @@
+count=0
+sum_number= 0
+while True :
+    a=int(input("enter a number"))
+    if a<=0 :
+        break
+    count+=1
+    sum_number+=a    
+    if count>0 :
+        average= sum_number / a
+        print(average)
+
+
+
+while True:
+    number= int(input("enter a number"))
+    jazr= number**(1/2)
+    if jazr%(1/2)==0:
+        print("true")
+    else:
+        print("false")
+    
+
+
+
+
+count=0
+while True:
+    number= int(input("enter number"))
+    if number % 2==0 :
+        print("true")
+    else:
+        print("false")
+        count+=1
+      
